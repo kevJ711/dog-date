9c0d802c1e3ad462fba3e58bdff1561f3d81e67a
Cindy <cinderg166@gmail.com>
2025-10-21 15:05:23 -0700

    Discover/Browse UIis done, not yet integrated into backend or rest of website UI

--- src/components/ui/DiscoveryCard.tsx
+++ src/components/ui/DiscoveryCard.tsx
@@ -2,28 +2,37 @@
 
 import Link from "next/link";
 
-export default function DiscoveryCard() {
-    //Test profile
-    const profile = [
-        "Bailey",
-        "Calm, affectionate, gentle",
-        "Mon-Fri 10:00am-2:00pm" ,
-        "Hi there! I'm Daisy, a 3-year-old Golden Retriever with a heart full of love and a tail that never stops wagging. I adore meeting new people and am always up for a game of fetch or a cozy cuddle session. I'm well-socialized with both dogs and kids, and I promise to greet you with a big, slobbery kiss!"];
+type DiscoveryCardProps = {
+    dogId: string;
+    dogName: string;
+    dogTemp: string;
+    avalibility: string;
+    dogBio: string;
+    className?: string;
+    likeButton: React.ReactNode;
+    expandButton: React.ReactNode;
+};
+
+export default function DiscoveryCard({dogId, dogName, dogTemp, avalibility, dogBio, likeButton, expandButton} : DiscoveryCardProps) {
    return(
-    <div className="p-8">
-        <div className="bg-white w-auto rounded-3xl shadow-lg;">
-                <div className="p-8"> 
-                    <h1 className="text-black text-xl font-mono font-light;" >Meet: </h1>
-                    <h2>{ profile[0] }</h2>
-                    <h1 className="text-black text-xl font-mono font-light;" >Temperment: </h1>
-                    <h2>{ profile[1] }</h2>
-                    <h1 className="text-black text-xl font-mono font-light;" >Times Avalible: </h1>
-                    <h2>{ profile[2] }</h2>
-                    <h1 className="text-black text-xl font-mono font-light;" >Bio: </h1>
-                    <h2>{ profile[3] }</h2> 
+    <div className="w-full h-full">
+        <div className="bg-white w-auto rounded-3xl shadow-lg border-2 p-8 pt-2 pb-2 ">
+                <div className="flex flex-row justify-end  bg-gray border-b p-3 border-gray-200">
+                    {expandButton}
                 </div>
-            <div id="bottomsection" className="bg-gray border-t p-3 border-gray-200">
-                <Link href="" className="text-center text-l">Request a playdate! → </Link>
+                <div className="p-6 h-64 overflow-y-auto"> 
+                    <h1 className="text-black text-xl font-mono font-light;" >Meet: </h1>
+                    <h2>{ dogName }</h2>
+                    <h1 className="text-black text-xl font-mono font-light;" >Temperment: </h1>
+                    <h2>{ dogTemp }</h2>
+                    <h1 className="text-black text-xl font-mono font-light;" >Times Avalible: </h1>
+                    <h2>{ avalibility }</h2>
+                    <h1 className="text-black text-xl font-mono font-light;" >Bio: </h1>
+                    <h2>{ dogBio }</h2> 
+                </div>
+            <div id="bottomsection" className="flex flex-row justify-between items-center bg-gray border-t p-3 border-gray-200">
+                <div>{likeButton}</div>
+                <Link href="" className="text-l">Request a playdate! → </Link>
             </div>          
         </div>
     </div>
